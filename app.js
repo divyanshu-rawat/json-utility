4f37695612ebdfa908247d1293b727a56e0fc2ba
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-12-06 12:44:44 +0530

    csv-to-json and support for result in json, csv added

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const promisify = require("promisify-node");
-const promisifyloadJsonFile = promisify("load-json-file");
-var fs = promisify("fs");
+const fs = promisify("fs");
+const csv = require("csvtojson");
+const csvFilePath = "./csvInput/csv_data.csv";
 
-promisifyloadJsonFile("data.json")
+csv()
+   .fromFile(csvFilePath)
    .then(json => {
       for (let j = 0; j < json.length; j++) {
          json[j].location = {
@@ -25,12 +27,33 @@ promisifyloadJsonFile("data.json")
       return json;
    })
    .then(json => {
-      fs.writeFile("./object.json", JSON.stringify(json, null, 4), err => {
-         if (err) {
-            console.error(err);
-            return;
+      fs.writeFile(
+         "./jsonOutput/output.json",
+         JSON.stringify(json, null, 4),
+         err => {
+            if (err) {
+               console.error(err);
+               return;
+            }
          }
-      }).then(function() {
-         console.log("Hurray! Operation Completed");
+      ).then(() => {
+         console.log("Hurray! JSON Generated");
+
+         const fs = require("fs");
+         const Json2csvTransform = require("json2csv").Transform;
+         const transformOpts = { highWaterMark: 16384, encoding: "utf-8" };
+
+         const input = fs.createReadStream("./jsonOutput/output.json", {
+            encoding: "utf8"
+         });
+         const output = fs.createWriteStream("./csvOutput/output.csv", {
+            encoding: "utf8"
+         });
+         const json2csv = new Json2csvTransform(transformOpts);
+         const processor = input.pipe(json2csv).pipe(output);
+
+         console.log("Hurray! CSV Generated");
       });
    });
+
+// You can also listen for events on the conversion and see how the header or the lines are coming out.
